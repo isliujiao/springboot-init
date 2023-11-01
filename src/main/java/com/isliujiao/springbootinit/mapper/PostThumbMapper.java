f9fc48aa5bb354220a794102ff2578939aeadccc
liujiao <liujiao@hsyuntai.com>
2023-11-01 13:48:23 +0800

    SpringBoot初始化模板

---
+++ src/main/java/com/isliujiao/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,15 @@
+package com.isliujiao.springbootinit.mapper;
+
+import com.isliujiao.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/test/java/com/isliujiao/springbootinit/MainApplicationTests.java
@@ -0,0 +1,22 @@
+package com.isliujiao.springbootinit;
+
+import com.isliujiao.springbootinit.config.WxOpenConfig;
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 主类测试
+ */
+@SpringBootTest
+class MainApplicationTests {
+
+    @Resource
+    private WxOpenConfig wxOpenConfig;
+
+    @Test
+    void contextLoads() {
+        System.out.println(wxOpenConfig);
+    }
+
+}
